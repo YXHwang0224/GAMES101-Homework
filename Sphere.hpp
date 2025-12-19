7c5aa4f2ad30d1c8774d59888428e2b5f839e942
YXHwang0224 <165121966+YXHwang0224@users.noreply.github.com>
2025-12-19 21:44:26 +0800

    Add files via upload

---
+++ Sphere.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "Object.hpp"
+#include "Vector.hpp"
+
+class Sphere : public Object
+{
+public:
+    // Sphere的构造函数很简陋，这里是显式曲面，判断光线是否与其相交比较简单
+    Sphere(const Vector3f& c, const float& r)
+        : center(c)
+        , radius(r)
+        , radius2(r * r)
+    {}
+
+    // 判断光线是否与球体相交
+    bool intersect(const Vector3f& orig, const Vector3f& dir, float& tnear, uint32_t&, Vector2f&) const override
+    {
+        // analytic solution
+        Vector3f L = orig - center;
+        float a = dotProduct(dir, dir);
+        float b = 2 * dotProduct(dir, L);
+        float c = dotProduct(L, L) - radius2;
+        float t0, t1;
+        if (!solveQuadratic(a, b, c, t0, t1))
+            return false;
+        if (t0 < 0)
+            t0 = t1;
+        if (t0 < 0)
+            return false;
+        tnear = t0;
+
+        return true;
+    }
+
+    // 碰撞点在物体表面的其他信息，如法线
+    void getSurfaceProperties(const Vector3f& P, const Vector3f&, const uint32_t&, const Vector2f&,
+                              Vector3f& N, Vector2f&) const override
+    {
+        // 法线从球心指向碰撞点
+        N = normalize(P - center);
+    }
+
+    Vector3f center;
+    float radius, radius2;
+};
